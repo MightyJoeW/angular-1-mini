7df37cd16d84b9dc7ebe3e4a7b2115341380f889
Joe Warren <joe@Joes-MacBook-Pro.local>
2017-12-05 15:24:21 -0600

    completed

---
+++ js/mainCtrl.js
@@ -0,0 +1,8 @@
+angular.module("app").controller("mainCtrl", function($scope) {
+  $scope.friends = ["Finn", "Aj", "Seth", "Sasha"];
+  //   $scopre.addFriend = function(list) {
+  //     $scope.friends.push(list);
+  //     $scope.addNewFriend = false;
+  //     $scope.newFriend = {}
+  //   };
+});
